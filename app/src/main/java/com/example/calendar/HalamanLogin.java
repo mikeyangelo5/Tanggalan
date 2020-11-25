916d3592103294bda03840eda973ecf9189e70a2
mikeyangelo5 <71859576+mikeyangelo5@users.noreply.github.com>
2020-11-26 02:42:55 +0700

    login edit

--- app/src/main/java/com/example/calendar/HalamanLogin.java
+++ app/src/main/java/com/example/calendar/HalamanLogin.java
@@ -9,15 +9,14 @@ import android.widget.Button;
 
 public class HalamanLogin extends AppCompatActivity {
     private static final String TAG = "MainActivity";
-    private Button btncalendar;
+    private Button gotoCalendar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btncalendar = (Button) findViewById(R.id.btncalendar);
+        gotoCalendar = (Button) findViewById(R.id.btnLogin);
 
-        btncalendar.setOnClickListener(new View.OnClickListener() {
-
+        gotoCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HalamanLogin.this, HalamanCalendar.class);

fd653f8de586329f81bc28e766c69ac29b8be3c7
ASUSTUF505GD\ASUS <angger.herlambang@ti.ukdw.ac.id>
2020-11-26 19:44:19 +0700

    Tambahan Date Picker pada Add Event

--- app/src/main/java/com/example/calendar/AddEvent.java
+++ app/src/main/java/com/example/calendar/AddEvent.java
@@ -2,11 +2,18 @@ package com.example.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class AddEvent extends AppCompatActivity {
     private static final String TAG = AddEvent.class.getSimpleName();
@@ -14,6 +21,9 @@ public class AddEvent extends AppCompatActivity {
     private Button cancelCreateEvent;
     private EditText titleEvent;
     private EditText addressEvent;
+    private TextView tvDate;
+    DatePickerDialog.OnDateSetListener setListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +32,7 @@ public class AddEvent extends AppCompatActivity {
         cancelCreateEvent = (Button) findViewById(R.id.btnCancel);
         titleEvent = (EditText) findViewById(R.id.input_titleEvent);
         addressEvent = (EditText) findViewById(R.id.input_Address);
+        tvDate = findViewById(R.id.tv_date);
 
         createEvent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,5 +49,29 @@ public class AddEvent extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(calendar.YEAR);
+        final int month = calendar.get(calendar.MONTH);
+        final int day = calendar.get(calendar.DAY_OF_MONTH);
+
+        tvDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        AddEvent.this, android.R.style.Theme_Holo_Light_Dialog_MinWidth
+                        , setListener, year, month, day);
+                datePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+                datePickerDialog.show();
+            }
+        });
+        setListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                String date = dayOfMonth + "/" + month + "/" + year;
+                tvDate.setText(date);
+            }
+        };
     }
 }

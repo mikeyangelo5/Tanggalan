f9e8b116f1fcf1861dfaf69c44de5f1a2d639db3
ASUSTUF505GD\ASUS <angger.herlambang@ti.ukdw.ac.id>
2020-11-26 17:52:33 +0700

    Tombol dan halaman SignUp

--- app/src/main/java/com/example/calendar/HalamanLogin.java
+++ app/src/main/java/com/example/calendar/HalamanLogin.java
@@ -10,16 +10,26 @@ import android.widget.Button;
 public class HalamanLogin extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Button gotoCalendar;
+    private Button gotoDaftar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         gotoCalendar = (Button) findViewById(R.id.btnLogin);
+        gotoDaftar = (Button) findViewById(R.id.btnDaftar);
 
         gotoCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HalamanLogin.this, NavigationDrawer.class);
+                startActivity(intent);
+            }
+        });
+
+        gotoDaftar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HalamanLogin.this, SignUp.class);
                 startActivity(intent);
             }
         });
